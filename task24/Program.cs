e7c05e204c6f73a3ecaef42914cb75996e3fd136
naumovanatalia <naumova.na43@mail.ru>
2022-12-13 21:47:40 +0200

    add task24 task26

---
+++ task24/Program.cs
@@ -0,0 +1,19 @@
+﻿//метод считывает целое число от пользователя( конвертирует введенные данные в число)
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+//возвращает сумму от 1 до number
+int GetSumToNumber(int number)
+{
+    int sum = 0;
+    for (int i =1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+int A = ReadInt("Введите число: ");
+int result = GetSumToNumber(A);
+Console.WriteLine($"Сумма чисел от 1 до {A} = {result}");
---
+++ task26/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (a != 0)
+{
+    a = a / 10; // a /= 10
+    count++;
+}
+Console.WriteLine($"в числе {count} цифр");

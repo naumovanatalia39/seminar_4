a858b691f512a58c07bed00b41fe46d9c045c36a
naumovanatalia <naumova.na43@mail.ru>
2022-12-13 21:59:32 +0200

    add task28

---
+++ task28/Program.cs
@@ -0,0 +1,18 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetProductNumbers (int number)
+{
+    int prod = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        prod = prod * i; 
+    }
+    return prod;
+}
+int a = ReadInt("Введите число");
+int result = GetProductNumbers(a);
+Console.WriteLine($"Сумма чисел от 1 до {a} = {result}");

483aa5cde7698ea74e31353747f5f20d0afc192a
naumovanatalia <naumova.na43@mail.ru>
2022-12-14 21:09:23 +0200

    add hometask27

---
+++ hometask27/Program.cs
@@ -0,0 +1,14 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = ReadInt("Введите число");
+int res = 0;
+while(a != 0)
+{
+    res = a % 10 + res;
+    a = a / 10;
+}
+Console.WriteLine($"Сумма цифр в числе {res}");

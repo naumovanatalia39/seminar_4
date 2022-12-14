71123e2e5f21e613d1d9e4d3c271e15a4cc3ef15
naumovanatalia <naumova.na43@mail.ru>
2022-12-14 21:18:12 +0200

    add hometask9

---
+++ hometask29/Program.cs
@@ -0,0 +1,12 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [] array = new int [8];
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = ReadInt("Введите число");
+}
+Console.WriteLine(string.Join(", ", array));

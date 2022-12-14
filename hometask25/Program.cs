57c03afadd02ea1eec1c800ef9dd5d6fab1a72f6
naumovanatalia <naumova.na43@mail.ru>
2022-12-14 20:37:26 +0200

    add hometask25

---
+++ hometask25/Program.cs
@@ -0,0 +1,13 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a = ReadInt("Введите первое число");
+int b = ReadInt("Введите второе число");
+int res = 1;
+for(int i = 1; i <= b; i++)
+{
+    res = res * a;
+}
+Console.WriteLine(res);
